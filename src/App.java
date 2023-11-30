261882d08266627a148ddbe6f2f3f117dc0213ba
Jose Maria Donoso Yerga <jmdonyer@gmail.com>
2023-11-30 09:12:24 +0100

    primer commit

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        
+        
+        System.out.println("Si ves esto, hemos compartido por GitHub yipiii");
+
+        /*
+            Pasos para subir un repositorio desde git con la consola
+
+         * Entramos con la consola en la carpeta del repositorio
+         * 
+         * git init para iniciar Git
+         * 
+         * git add . -> Para meter los archivos en el respositorio
+         * 
+         * git status -> te muestra los archivos que se subiran, si sale en rojo, hay que volver a git add . por que se han modificado los archivos
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+
+
+
+
+    }
+}
